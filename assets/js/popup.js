00a09ada5e007f12cc9a032d352ab4a1a06336cc
林宛慧 <aa0960757329@gmail.com>
2021-11-19 15:19:48 +0800

    1119_英文版final

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,7 +1,90 @@
+// 上半場講者
 $('.open_popup--1').click(function(){
   $('.popupBox--1').fadeIn();
   $("body").addClass("body_fixed");
 });
+$('.open_popup--2').click(function(){
+  $('.popupBox--2').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--3').click(function(){
+  $('.popupBox--3').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--4').click(function(){
+  $('.popupBox--4').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--5').click(function(){
+  $('.popupBox--5').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--6').click(function(){
+  $('.popupBox--6').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--7').click(function(){
+  $('.popupBox--7').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--8').click(function(){
+  $('.popupBox--8').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--9').click(function(){
+  $('.popupBox--9').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--10').click(function(){
+  $('.popupBox--10').fadeIn();
+  $("body").addClass("body_fixed");
+});
+// 下半場講者
+$('.open_popup--11').click(function(){
+  $('.popupBox--6').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--12').click(function(){
+  $('.popupBox--12').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--13').click(function(){
+  $('.popupBox--13').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--14').click(function(){
+  $('.popupBox--14').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--15').click(function(){
+  $('.popupBox--15').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--16').click(function(){
+  $('.popupBox--16').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--17').click(function(){
+  $('.popupBox--17').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--18').click(function(){
+  $('.popupBox--18').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--19').click(function(){
+  $('.popupBox--19').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--20').click(function(){
+  $('.popupBox--20').fadeIn();
+  $("body").addClass("body_fixed");
+});
+$('.open_popup--21').click(function(){
+  $('.popupBox--21').fadeIn();
+  $("body").addClass("body_fixed");
+});
+
 $('.open_cathay').click(function(){
   $('.popupBox--cathay').fadeIn();
   $("body").addClass("body_fixed");
